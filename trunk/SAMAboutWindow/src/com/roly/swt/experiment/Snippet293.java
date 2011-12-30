8caa5822b29ce938a9cbec00a34b6b043170d1b0
rollandsov@gmail.com <rollandsov@gmail.com@a539c892-cc37-6f66-8264-d56c9aaab9b4>
2011-12-30 23:05:03 +0000

    Working on AboutBox with SWT

--- trunk/SAMAboutWindow/src/com/roly/swt/experiment/Snippet293.java
+++ trunk/SAMAboutWindow/src/com/roly/swt/experiment/Snippet293.java
@@ -1,43 +1,48 @@
 package com.roly.swt.experiment;
 
-import org.eclipse.swt.*;
-import org.eclipse.swt.graphics.*;
-import org.eclipse.swt.layout.*;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.*;
 import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.layout.FillLayout;
 
 public class Snippet293 {
-	public static void main (String [] args) {
-		Display display = new Display ();
-		Shell shell = new Shell (display, SWT.SHELL_TRIM | SWT.DOUBLE_BUFFERED);
-		shell.setLayout(new FillLayout ());
-		final Image image = display.getSystemImage (SWT.ICON_QUESTION);
-		shell.addListener (SWT.Paint, new Listener () {
-			public void handleEvent (Event e) {
-				Rectangle rect = image.getBounds ();
-				GC gc = e.gc;
-				int x = 50, y = 40;
-				gc.drawImage (image, x, y);
-				Transform tr = new Transform (e.display);
-				//tr.setElements (1, 0, 0, -1, 1, 2*(y+rect.height));
-				tr.setElements (1, 0, 0, -1, 1, 2*(y+rect.height));
-				gc.setTransform (tr);
-				gc.drawImage (image, x, y);
-				gc.setTransform (null);
-				Color background = gc.getBackground ();
-				Pattern p = new Pattern (e.display, x, y+rect.height, x, y+(2*rect.height), background, 0, background, 255);
-				gc.setBackgroundPattern (p);
-				gc.fillRectangle (x, y+rect.height, rect.width, rect.height);
-				p.dispose ();
-				tr.dispose ();
+	Display display = new Display();
+	Shell shell = new Shell(display);
+
+	public Snippet293() {
+		shell.setLayout(new FillLayout());
+		shell.setText("Tabs");
+
+		final TabFolder tabFolder = new TabFolder(shell, SWT.NONE);
+		for (int i = 1; i < 5; i++) {
+			TabItem item = new TabItem(tabFolder, SWT.NULL);
+			item.setText("Tab" + i);
+			Text text = new Text(tabFolder, SWT.BORDER | SWT.MULTI);
+			text.setText("This is Tab " + i);
+			item.setControl(text);
+		}
+		tabFolder.setSize(850, 850);
+		tabFolder.addSelectionListener(new SelectionListener() {
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println("You have selected:" + tabFolder.getSelection()[0].toString());
+			}
+
+			public void widgetDefaultSelected(SelectionEvent e) {
+				widgetSelected(e);
 			}
 		});
-		shell.setSize (200, 200);
-		shell.open ();
-		while (!shell.isDisposed ()) {
-			if (!display.readAndDispatch ())
-				display.sleep ();
+		shell.setSize(1300, 950);
+		shell.open();
+
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
 		}
-		display.dispose ();
+		display.dispose();
 	}
-	
+
+	public static void main(String[] args) {
+		new Snippet293();
+	}
 }
